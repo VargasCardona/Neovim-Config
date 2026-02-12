b4c19cc0dd6529381aef40a889add80f9404a34d
Nicolas <vargascardona12@proton.me>
2026-02-12 00:40:03 -0500

    docs: add comments

--- lua/sieg/lazy.lua
+++ lua/sieg/lazy.lua
@@ -1,9 +1,11 @@
--- Bootstrap lazy.nvim
+-- Bootstrap lazy.nvim - the modern plugin manager for Neovim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  -- Clone lazy.nvim if it doesn't exist yet
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
+    -- Display error message and exit if clone fails
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
@@ -13,57 +15,58 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
+-- Add lazy.nvim to the runtime path so it can be loaded
 vim.opt.rtp:prepend(lazypath)
 
+-- Plugin configuration - list of all plugins to be managed by lazy.nvim
 local plugins = {
-  'nvim-lua/plenary.nvim',
-  'mbbill/undotree',
+  'nvim-lua/plenary.nvim',           -- Utility library for Neovim plugins (required by many plugins)
+  'mbbill/undotree',                 -- Visual undo history navigator
   {
-      "ThePrimeagen/harpoon",
-      branch = "harpoon2",
+      "ThePrimeagen/harpoon",        -- Quick navigation between files/bookmarks
+      branch = "harpoon2",           -- Use the newer harpoon2 branch
       dependencies = { "nvim-lua/plenary.nvim" }
   },
-  "barrett-ruth/live-server.nvim",
-  "windwp/nvim-ts-autotag",
-  "turbio/bracey.vim",
+  "barrett-ruth/live-server.nvim",   -- Live development server for web development
+  "windwp/nvim-ts-autotag",          -- Auto-close/rename HTML/JSX tags using treesitter
+  "turbio/bracey.vim",               -- Live browser preview for HTML/CSS/JS
   {
-      'smoka7/hop.nvim',
+      'smoka7/hop.nvim',             -- Easy motion navigation plugin (like easymotion)
       version = "*",
       opts = {
-          keys = 'etovxqpdygfblzhckisuran'
+          keys = 'etovxqpdygfblzhckisuran' -- Custom hop keys for navigation
       }
   },
-  'rafamadriz/friendly-snippets',
-  'mfussenegger/nvim-jdtls',
+  'rafamadriz/friendly-snippets',   -- Collection of snippets for various languages
+  'mfussenegger/nvim-jdtls',         -- Java development tools language server
   {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
+      'nvim-lualine/lualine.nvim',   -- Status line plugin
+      dependencies = { 'nvim-tree/nvim-web-devicons' } -- File icons for status line
   },
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'windwp/nvim-autopairs',
+  'hrsh7th/nvim-cmp',                -- Completion engine
+  'hrsh7th/cmp-nvim-lsp',            -- LSP completion source for nvim-cmp
+  'windwp/nvim-autopairs',           -- Auto-close brackets, quotes, etc.
 	{
-			'nvim-telescope/telescope.nvim', version = '*',
+			'nvim-telescope/telescope.nvim', version = '*', -- Fuzzy finder, file browser, etc.
 			dependencies = {
-					'nvim-lua/plenary.nvim',
-					{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+					'nvim-lua/plenary.nvim', -- Required dependency
+					{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }, -- Faster sorting algorithm
 			}
 	},
-  "ellisonleao/gruvbox.nvim",
+  "ellisonleao/gruvbox.nvim",        -- Gruvbox color scheme
   { 
-	  	"kabouzeid/nvim-jellybeans",
-			dependencies = "rktjmp/lush.nvim"
+	  	"kabouzeid/nvim-jellybeans",   -- Jellybeans color scheme
+			dependencies = "rktjmp/lush.nvim" -- Color scheme creation framework
 	},
-  'kvrohit/rasmus.nvim',
-  "rebelot/kanagawa.nvim",
-  "slugbyte/lackluster.nvim",
+  'kvrohit/rasmus.nvim',             -- Rasmus color scheme
+  "rebelot/kanagawa.nvim",           -- Kanagawa color scheme (inspired by Japanese paintings)
+  "slugbyte/lackluster.nvim",        -- Minimal color scheme
 }
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- Set up leader keys before loading lazy.nvim to ensure plugin mappings work correctly
+-- Leader keys are used for custom keybindings (leader for global, localleader for buffer-local)
+vim.g.mapleader = " "               -- Use space as the global leader key
+vim.g.maplocalleader = "\\"         -- Use backslash as the local leader key
 
--- Setup lazy.nvim
-require("lazy").setup(plugins, {})
+-- Initialize lazy.nvim with the plugin list
+require("lazy").setup(plugins, {})   -- Load all configured plugins
--- lua/sieg/remap.lua
+++ lua/sieg/remap.lua
@@ -1,25 +1,32 @@
+-- =============================================================================
+-- KEYBINDINGS AND OPTIONS CONFIGURATION
+-- =============================================================================
+
+-- Set leader key to space
 vim.g.mapleader = " "
 
-
+-- =============================================================================
+-- TAB BEHAVIOR
+-- =============================================================================
+-- Command mode: Tab exits to normal mode
 vim.keymap.set("c", "<Tab>", "<C-C><Esc>")
+-- Insert mode: Tab exits to normal mode and stays at cursor position
 vim.keymap.set("i", "<Tab>", "<Esc>`^")
 
+-- =============================================================================
+-- PYTHON EXECUTION
+-- =============================================================================
+-- F5: Save and run current Python file with output capture
 vim.keymap.set('n', '<F5>', function()
     vim.cmd('w')
-
-    -- Get the current file's path
     local file_path = vim.fn.expand('%')
-
-    -- Run the Python script in the background
     vim.fn.jobstart('python ' .. file_path, {
         on_stdout = function(_, data, _)
-            -- Print the output to the Neovim message area
             if data then
                 vim.api.nvim_echo({ { table.concat(data, '\n'), 'Normal' } }, false, {})
             end
         end,
         on_stderr = function(_, data, _)
-            -- Print errors to the Neovim message area
             if data then
                 vim.api.nvim_echo({ { table.concat(data, '\n'), 'ErrorMsg' } }, false, {})
             end
@@ -27,47 +34,92 @@ vim.keymap.set('n', '<F5>', function()
     })
 end, { noremap = true, silent = false })
 
+-- =============================================================================
+-- TAB KEY MODIFIERS
+-- =============================================================================
+-- CapsLock acts as Tab in insert mode
 vim.api.nvim_set_keymap("i", "<CapsLock>", "<Tab>", { noremap = true, silent = true })
+-- CapsLock enters insert mode and inserts Tab
 vim.api.nvim_set_keymap("n", "<CapsLock>", "i<Tab>", { noremap = true, silent = true })
+-- F13 acts as Tab in insert mode
 vim.api.nvim_set_keymap("i", "<F13>", "<Tab>", { noremap = true, silent = true })
 
+-- =============================================================================
+-- CLIPBOARD AND MOUSE
+-- =============================================================================
+-- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
+-- Disable mouse support
+vim.opt.mouse = ""
 
+-- =============================================================================
+-- NAVIGATION DISABLING
+-- =============================================================================
+-- Disable arrow keys in normal mode to force hjkl usage
 vim.keymap.set("n", "<Up>", "<Nop>")
 vim.keymap.set("n", "<Down>", "<Nop>")
 vim.keymap.set("n", "<Left>", "<Nop>")
 vim.keymap.set("n", "<Right>", "<Nop>")
-
+-- Disable some arrow keys in insert mode
 vim.keymap.set("i", "<Up>", "<Nop>")
 vim.keymap.set("i", "<Left>", "<Nop>")
 vim.keymap.set("i", "<Right>", "<Nop>")
 
-vim.opt.mouse = ""
-
+-- =============================================================================
+-- SCROLLING
+-- =============================================================================
+-- Keep cursor centered when scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- =============================================================================
+-- FILE OPERATIONS
+-- =============================================================================
+-- Ctrl+p: Save and run Python in split terminal
 vim.keymap.set("n", "<C-p>", ":w <CR> :sp <CR> :term python3 % <CR> <Esc>i")
+-- Ctrl+ho: Save and open file in Firefox
 vim.keymap.set("n", "<C-ho", ":w <CR> :silent !firefox % <CR>")
-
+-- Ctrl+s: Save file
 vim.keymap.set("n", "<C-s>", ":w <CR>")
 
-vim.keymap.set("n", "<leader>w", ":HopWord <CR>" )
-vim.keymap.set("x", "<leader>w", ":HopWord <CR>" )
+-- =============================================================================
+-- MOVEMENT AND SEARCH
+-- =============================================================================
+-- Leader+w: Hop to word (requires hop.nvim plugin)
+vim.keymap.set("n", "<leader>w", ":HopWord <CR>")
+vim.keymap.set("x", "<leader>w", ":HopWord <CR>")
 
+-- =============================================================================
+-- VISUAL MODE
+-- =============================================================================
+-- Keep selection when indenting
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
 
-vim.opt.shiftwidth = 2 
-vim.opt.tabstop = 2 
+-- =============================================================================
+-- EDITOR OPTIONS
+-- =============================================================================
+-- Indentation
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
+
+-- Line numbers
 vim.opt.relativenumber = true
 vim.opt.nu = true
+
+-- Display
 vim.opt.wrap = false
-vim.opt.backup = false
-vim.opt.swapfile = false
+vim.opt.termguicolors = true
+
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.termguicolors = true
+
+-- Performance
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+-- Scrolling and timing
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
